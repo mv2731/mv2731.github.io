ef79451ea94977d8edc90bdb2ab5eaa1f0b79d6f
Maya Venkatraman <mvenkat@google.com>
2025-05-26 19:24:32 -0400

    Add files for next js project

---
+++ maya-portfolio/publications.js
@@ -0,0 +1,10 @@
+function PublicationsPage() {
+  return (
+    <div>
+      <h1>Publications</h1>
+      <p>This is where your publications will go.</p>
+    </div>
+  );
+}
+
+export default PublicationsPage;
---
+++ maya-portfolio/reads.js
@@ -0,0 +1,10 @@
+function ReadsPage() {
+  return (
+    <div>
+      <h1>Reads</h1>
+      <p>This is where the books you've read will be listed.</p>
+    </div>
+  );
+}
+
+export default ReadsPage;
